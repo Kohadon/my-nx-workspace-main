7a0401e67c92daebf031cd89383c6ab0e793a2fe
2aiconcept <contact@2aiconcept.com>
2025-11-30 22:50:09 +0100

    test app

--- apps/mini-crm/src/app/app.component.spec.ts
+++ apps/mini-crm/src/app/app.component.spec.ts
@@ -1,1 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { AppComponent } from './app.component';
 
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideZonelessChangeDetection()],
+    }).compileComponents();
+  });
+
+  it('should create the app', async () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    await fixture.whenStable();
+    expect(app).toBeTruthy();
+  });
+});
